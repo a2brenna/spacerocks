ad9fae68380fa59406167c10ffe6a4b2905e1e3a
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2016-04-21 22:21:37 -0400

    Add types for more safer physics calculations

---
+++ src/physics.h
@@ -0,0 +1,31 @@
+#ifndef __PHYSICS_H__
+#define __PHYSICS_H__
+
+class Position {
+
+    public:
+
+        int x;
+        int y;
+
+};
+
+class Displacement {
+
+    public:
+
+        int x;
+        int y;
+
+};
+
+class Velocity {
+
+    public:
+
+        int x;
+        int y;
+
+};
+
+#endif
