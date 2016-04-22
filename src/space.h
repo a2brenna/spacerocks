3760ab53c47250c4e65b817b430d2e653a10b4be
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2016-04-21 22:11:23 -0400

    Add a container for tracking the Objects in Space

---
+++ src/space.h
@@ -0,0 +1,17 @@
+#ifndef __SPACE_H__
+#define __SPACE_H__
+
+#include <vector>
+#include <memory>
+
+#include "object.h"
+
+class Space{
+
+    private:
+
+        std::vector<Object> _floaters;
+
+};
+
+#endif
