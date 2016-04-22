2e9e1aa5fb2f58344bf5a2135317d8339fa9dbfa
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2016-04-21 23:05:29 -0400

    Helpful documentation on how physics works

--- src/physics.h
+++ src/physics.h
@@ -1,6 +1,11 @@
 #ifndef __PHYSICS_H__
 #define __PHYSICS_H__
 
+//All physics calculations are done in a large but finite square 2^64 x 2^64
+//Positions of Objects are squashed into pixel values as a last step before
+//rendering by dividing x and y coordinates by a constant factor to scale down
+//to screen resolution.
+
 #include <stdint.h>
 
 class Position {
